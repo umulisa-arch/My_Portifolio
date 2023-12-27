f8fe2fa5b6ac4cb02b9d44e73649e4478eb0e6ba
Agnes RUKUNDO <agnesrukundo1998@gmail.com>
2023-12-27 14:18:03 +0200

    change made on form

--- contact.js
+++ contact.js
@@ -6,6 +6,23 @@ document.getElementById('sendButton').addEventListener('click', function (event)
   const message = document.getElementById('getmessage').value;
 
   // Send email using mailto
-  const mailtoLink = `mailto:agnesrukundo1998@gmail.com?subject=Message from ${name} &body=${message}%0A%0AReply to: ${email}`;
+  const mailtoLink = `mailto:agnesrukundo1998@gmail.com?subject=Message from ${name} : ${subject} &body=${message}%0A%0AReply to: ${email}`;
   window.location.href = mailtoLink;
+  // return alert("Your data sent well!!");
+
+
+  // function sendMessage() {
+  //   var Name = document.getElementById('Name').value;
+  //   var email = document.getElementById('email').value;
+  //   var subject = document.getElementById('subject').value;
+  //   var message = document.getElementById('message').value;
+    
+  //   alert('Name: ' + Name + '\nEmail: ' + email + '\nsubject: ' + subject +
+  //         '\nmessage: ' + message );
+  
+   
+  // }
+  // sendMessage();
+
 });
+

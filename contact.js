c98a43fbcc9e7a7ba90ca0cd2d6ca7fe325a31cf
Agnes RUKUNDO <agnesrukundo1998@gmail.com>
2023-12-27 10:17:36 +0200

    update

--- contact.js
+++ contact.js
@@ -1,10 +1,14 @@
-function displayMessage() {
-    // var inputField = document.getElementById("myInput");
-    // var message = document.getElementById("message");
+document.getElementById('myForm').addEventListener('submit', function(e) {
+  e.preventDefault();
+  var formData = new FormData(this);
   
-    if (inputField.value.trim() !== '') {
-      message.style.display = "block";
-    } else {
-      message.style.display = "none";
-    }
-  }
+  fetch('https://script.google.com/macros/s/AKfycbx8TKBhPyQWk3g2OsS1eJu4jJQfnrG7SAQ33qt69AnTTrGVc4e6aHvizpNJ141RB_mkNQ/exec', {
+    method: 'POST',
+    body: formData
+  })
+  .then(response => {
+    console.log('Form submitted successfully');
+    // Handle success or redirect user
+  })
+  .catch(error => console.error('Error:', error));
+});

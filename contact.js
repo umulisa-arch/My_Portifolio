066fcb7b0a72957e7785772386e193541fe7c8f7
Agnes RUKUNDO <agnesrukundo1998@gmail.com>
2023-12-27 10:38:16 +0200

    update

--- contact.js
+++ contact.js
@@ -1,16 +1,9 @@
- function submitToGoogleSheet(){
-document.getElementById('myForm').addEventListener('submit', function(e) {
-  e.preventDefault();
-  var formData = new FormData(this);
-  
-  fetch('https://script.google.com/macros/s/AKfycbx8TKBhPyQWk3g2OsS1eJu4jJQfnrG7SAQ33qt69AnTTrGVc4e6aHvizpNJ141RB_mkNQ/exec', {
-    method: 'POST',
-    body: formData
-  })
-  .then(response => {
-    console.log('Form submitted successfully');
-    // Handle success or redirect user
-  })
-  .catch(error => console.error('Error:', error));
+document.getElementById('sendButton').addEventListener('click', function (event) {
+  event.preventDefault();
+  const name = document.getElementById('nameInput').value;
+  const email = document.getElementById('emailInput').value;
+  const message = document.getElementById('messageInput').value;
+  // Send email using mailto
+  const mailtoLink = `mailto:agnesrukundo1998@gmail.com?subject=Message from ${name}&body=${message}%0A%0AReply to: ${email}`;
+  window.location.href = mailtoLink;
 });
-}

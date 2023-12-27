2d8d23d1deb94c805e3ce569695f677a350cf0d1
Agnes RUKUNDO <agnesrukundo1998@gmail.com>
2023-12-27 10:41:17 +0200

    update

--- contact.js
+++ contact.js
@@ -1,8 +1,10 @@
 document.getElementById('sendButton').addEventListener('click', function (event) {
   event.preventDefault();
-  const name = document.getElementById('nameInput').value;
-  const email = document.getElementById('emailInput').value;
-  const message = document.getElementById('messageInput').value;
+  const name = document.getElementById('getname').value;
+  const email = document.getElementById('getemail').value;
+  const subject = document.getElementById('getsubject').value;
+  const message = document.getElementById('getmessage').value;
+
   // Send email using mailto
   const mailtoLink = `mailto:agnesrukundo1998@gmail.com?subject=Message from ${name}&body=${message}%0A%0AReply to: ${email}`;
   window.location.href = mailtoLink;
